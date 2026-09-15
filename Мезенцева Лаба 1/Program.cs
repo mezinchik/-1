0a5f1b0a037be16cb0032a094a5f04403924088c
Moroz07 <morozov-f.morozov@yandex.ru>
2026-09-15 09:04:38 +0300

    ввели аргументы

--- Мезенцева Лаба 1/Program.cs
+++ Мезенцева Лаба 1/Program.cs
@@ -13,6 +13,9 @@ namespace Мезенцева_Лаба_1
             double orderCost = Class1.ReadDouble("Введите стоимость заказа (руб.): ", 0);
             double distance = Class1.ReadDouble("Введите расстояние доставки (км): ", 0);
             int hour = Class1.ReadInt("Введите время заказа (час): ", 0, 23);
+
+            double delivery = Class1.CalcDelivery(orderCost, distance, hour);
+            double total = orderCost + delivery;
         }
     }
 }
