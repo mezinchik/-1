9c531efd1c0ac5aa13b58828c42661fee590dc29
Moroz07 <morozov-f.morozov@yandex.ru>
2026-09-15 09:08:52 +0300

    прописали образование цены

--- Мезенцева Лаба 1/Class1.cs
+++ Мезенцева Лаба 1/Class1.cs
@@ -10,7 +10,16 @@ namespace Мезенцева_Лаба_1
     {
         public static double CalcDelivery(double orderCost, double distance, int hour)
         {
+            if (orderCost >= 2000) return 0;
 
+            double basePrice = 150;
+            if (distance > 3)
+                basePrice += (distance - 3) * 50;
+
+            if (IsPeakHour(hour))
+                basePrice *= 1.3;
+
+            return basePrice;
         }
     }
 }
