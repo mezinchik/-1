a1dcd4052ce5bc1d4e9705be6bde6a0bcad918f4
Moroz07 <morozov-f.morozov@yandex.ru>
2026-09-15 09:03:46 +0300

    ввели расстояние доставки и время заказа

--- Мезенцева Лаба 1/Program.cs
+++ Мезенцева Лаба 1/Program.cs
@@ -11,6 +11,8 @@ namespace Мезенцева_Лаба_1
         static void Main(string[] args)
         {
             double orderCost = Class1.ReadDouble("Введите стоимость заказа (руб.): ", 0);
+            double distance = Class1.ReadDouble("Введите расстояние доставки (км): ", 0);
+            int hour = Class1.ReadInt("Введите время заказа (час): ", 0, 23);
         }
     }
 }
